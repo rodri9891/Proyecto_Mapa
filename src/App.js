ff4d084a45f161cc2b37d32c5c3daacfaed0d778
rodri9891 <42526612+rodri9891@users.noreply.github.com>
2018-10-28 00:05:50 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,8 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
-            no se efectua el cambio
+          Cambios efectuados
           </a>
         </header>
       </div>
